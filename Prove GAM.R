740ad41a1fdbc53567cdaeae785a7881d1fd78fc
DanielaZanotti <danielazanotti.98@gmail.com>
2021-12-06 17:03:31 +0100

    Update gam

--- Prove GAM.R
+++ Prove GAM.R
@@ -3,19 +3,39 @@ library(gamm4)
 #fgamm <- gamm(pressure ~ s(u_in, bs="cr") + s(time_step, bs="cr"), random = list(breath_id=~1), data = ts)
 
 
-#m <- gam(pressure ~  max_u_in + tot_u_in + R_C + s(u_in, bs = 're') + s(u_in, by = breath_id) + s(time_step, by = breath_id) +
-#           + s(tot_u_in, by = breath_id) + s(max_u_in, by = breath_id),
-#         family=gaussian, data=train_until1, method = "REML")
-#summary(m)
-#R_adj = 0.714   deviance explained = 71.4%
-#plot(m)
+############
+### GAM
+############
+
+# bs = 're' o 'cr'
+
+m <- gam(pressure ~ time_step + s(u_in) + R_C + tot_u_in + max_u_in + u_in_diff2 + 
+           + s(time_step, by = breath_id) + s(u_in, by = breath_id) + 
+           + s(tot_u_in, by = breath_id) +  s(last_u_in, by = breath_id) + s(first_u_in, by = breath_id) +
+           + s(max_u_in, by = breath_id) + s(u_in_diff1, by = breath_id) + s(u_in_diff2, by = breath_id) +
+           + s(u_in_diff_max, by = breath_id) + s(n_change_sign_u_in, by = breath_id) ,
+         family=gaussian, data=train_until1, method = "REML")
+summary(m)
+# R-sq.(adj) =  0.811   Deviance explained = 81.3%
+# -REML =  25293  Scale est. = 15.721    n = 8940
 
 
-fm1<-gamm4(pressure ~ max_u_in + tot_u_in + R_C + s(u_in) ,data=train_until1,
-           random = ~ (u_in + time_step + tot_u_in + max_u_in | breath_id))
+###########
+### GAMM4 
+###########
 
-summary(fm1$gam) ## summary of gam
-summary(fm1$mer) ## underlying mixed model
+fm<-gamm4(pressure ~ time_step + s(u_in) + R_C + tot_u_in + max_u_in + u_in_diff2 ,
+           data=train_until1,
+           random = ~ (time_step + u_in + u_out + tot_u_in + last_u_in + max_u_in | breath_id))
+
+fm1<-gamm4(pressure ~ time_step + s(u_in) + R_C + tot_u_in + max_u_in + u_in_diff2 ,
+           data=train_until1,
+           random = ~ (time_step + u_in + u_out + tot_u_in + max_u_in | breath_id))
+
+summary(fm$gam) ## summary of gam
+summary(fm$mer) ## underlying mixed model
+
+anova(fm$mer, fm1$mer)   #fm1 meglio 
 
 
 

e1eff0fa2500b12aff392379c02352985b99319f
DanielaZanotti <danielazanotti.98@gmail.com>
2021-12-11 17:29:31 +0100

    Update comparison between models

--- Prove GAM.R
+++ Prove GAM.R
@@ -25,17 +25,80 @@ pr <- predict.gam(m, test_until1)
 ### GAMM4 
 ###########
 
-fm1 <- gamm4(pressure ~ s(u_in) + R + C + tot_u_in + u_in_diff1 + u_in_diff2 ,
+#gamm with random intercept by breath_id
+m1 <- gamm4(pressure ~ s(u_in) + tot_u_in + u_in_diff1 + u_in_diff2 + factor(clust) +
+                       + factor(R_C == '20_20') + factor(R_C == '20_50') + factor(R_C == '5_20') + factor(R_C == '5_50'),
              data=train_until1,
-             random = ~ (1| breath_id) + (1| clust))
+             random = ~ (1| breath_id))
 
-summary(fm1$gam) ## summary of gam
-summary(fm1$mer) ## underlying mixed model
+#gamm with random intercept by clust
+m2 <- gamm4(pressure ~ s(u_in) + tot_u_in + u_in_diff1 + u_in_diff2 + factor(clust) +
+              + factor(R_C == '20_20') + factor(R_C == '20_50') + factor(R_C == '5_20') + factor(R_C == '5_50'),
+            data=train_until1,
+            random = ~ (1| clust))
 
-#R-sq.(adj) =  0.5
+#gamm with random intercept by breath_id & clust
+m3 <- gamm4(pressure ~ s(u_in) + tot_u_in + u_in_diff1 + u_in_diff2 + factor(clust) +
+                       + factor(R_C == '20_20') + factor(R_C == '20_50') + factor(R_C == '5_20') + factor(R_C == '5_50'),
+            data=train_until1,
+            random = ~ (1| breath_id) + (1| clust))
+
+summary(m3$gam) ## summary of gam
+summary(m3$mer) ## underlying mixed model
+
 
 #F RATIO 
 (27.4+19.06)/19.06
+
+mods <- list(m1, m2, m3)
+names(mods) <- c("m1", "m2", "m3")
+
+#### residuals gam (uguale per tutti i modelli)
+res <- lapply(mods, function(x) data.frame(res = residuals(x$gam), pred = predict(x$gam)))
+mod <- rep(names(mods), unlist(lapply(res, nrow)))
+res <- do.call("rbind", res)
+res$mod <- mod
+
+x11()
+hist(res[which(res$mod=='m3'), 'res'], xlab = '', main = 'GAM term residuals distribution')
+
+x11()
+qqnorm(res[which(res$mod=='m3'), 'res'], main = 'GAM term residuals quantile plot')
+qqline(res[which(res$mod=='m3'), 'res'], col='blue')
+
+x11()
+plot(res[which(res$mod=='m3'), 'pred'], res[which(res$mod=='m3'), 'res'], 
+     xlab = 'pred', ylab = 'res', main = 'GAM term residuals versus predictions')
+
+#### residuals random effects
+res_re <- lapply(mods, function(x) data.frame(res_re = residuals(x$mer), pred = predict(x$mer)))
+mod_re <- rep(names(mods), unlist(lapply(res_re, nrow)))
+res_re <- do.call("rbind", res_re)
+res_re$mod <- mod_re
+
+x11()
+par(mfrow=c(1,3))
+hist(res_re[which(res_re$mod=='m1'), 'res_re'], xlab = 'Model 1', main = '')
+hist(res_re[which(res_re$mod=='m2'), 'res_re'], xlab = 'Model 2', main = 'Random effect residuals distribution by model')
+hist(res_re[which(res_re$mod=='m3'), 'res_re'], xlab = 'Model 3', main = '')
+
+x11()
+par(mfrow=c(3,1))
+qqnorm(res_re[which(res_re$mod=='m1'), 'res_re'], main = '')
+qqline(res_re[which(res_re$mod=='m1'), 'res_re'], col='blue')
+qqnorm(res_re[which(res_re$mod=='m2'), 'res_re'], main = 'Random effect residuals quantile plot by model')
+qqline(res_re[which(res_re$mod=='m2'), 'res_re'], col='blue')
+qqnorm(res_re[which(res_re$mod=='m3'), 'res_re'], main = '')
+qqline(res_re[which(res_re$mod=='m3'), 'res_re'], col='blue')
+
+x11()
+par(mfrow=c(1,3))
+plot(res[which(res$mod=='m3'), 'pred'], res[which(res$mod=='m1'), 'res_re'], 
+     xlab = 'pred model 1', ylab = 'res', main = '')
+plot(res[which(res$mod=='m3'), 'pred'], res[which(res$mod=='m2'), 'res_re'], 
+     xlab = 'pred model 2', ylab = 'res', main = 'Random effect residuals versus predictions by model')
+plot(res[which(res$mod=='m3'), 'pred'], res[which(res$mod=='m3'), 'res_re'], 
+     xlab = 'pred model 3', ylab = 'res', main = '')
 
 ####
 # plot
